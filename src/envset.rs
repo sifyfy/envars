8918d6ff0b6a1eac0015a44cc6b0c1defde991af
siphilia <siphilia.rn@gmail.com>
2016-04-29 17:55:33 +0900

    EnvSetがyamlファイルを抽象化する型として使える様にする為の機能を追加している途中

--- src/envset.rs
+++ src/envset.rs
@@ -20,11 +20,57 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+use std::collections::BTreeMap;
+use std::io;
+use yaml_rust as yaml;
+
+/// `EnvSet`の名前。`ENV_SET_NAME.yaml`としてファイル名に用いられる。
 #[derive(Clone, Debug, AsRef, Eq, PartialEq)]
-pub struct EnvSet(String);
+pub struct EnvSetName(String);
+
+impl EnvSetName {
+    /// 半角英数字文字と _ 及び - から成る文字列を引数に与えた場合に`Some(env_set)`が返る。
+    /// それ以外の文字列の場合は`None`が返る。
+    pub fn new(name: &str) -> Option<EnvSetName> {
+        if name.chars()
+               .all(|c| c.len_utf8() == 1 && (c.is_alphanumeric() || c == '_' || c == '-')) {
+            Some(EnvSetName(name.to_string()))
+        } else {
+            None
+        }
+    }
+}
+
+pub enum EnvSetError {
+    IO(io::Error),
+    Yaml(yaml::ScanError),
+}
+
+/// `EnvSet`のメモリ上での表現型。
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct EnvSet {
+    name: EnvSetName,
+    set: BTreeMap<yaml::Yaml, yaml::Yaml>,
+}
 
 impl EnvSet {
-    pub fn new(env_set_name: &str) -> EnvSet {
-        EnvSet(env_set_name.to_string())
+    pub fn new(env_set_name: &EnvSetName) -> Result<EnvSet, EnvSetError> {
+        read_env_set_yaml(env_set_name).map(|env_set_yaml| {
+            EnvSet {
+                name: env_set_name.clone(),
+                set: env_set_yaml,
+            }
+        })
+    }
+
+    fn read_env_set_yaml(env_set_name: &env_set_name) -> Result<BTreeMap<yaml::Yaml, yaml::Yaml>, EnvSetError> {
+        let result: Result<String, io::Error> = unimplemented!();
+        result.map_err(|io_err| EnvSetError::IO(io_err)).and_then(|content| {
+            yaml::YamlLoader::load_from_str(&content)
+                .map(|vec_yaml| {
+                    vec_yaml.pop().and_then(|yaml| yaml.as_hash()).unwrap_or(BTreeMap::new())
+                })
+                .map_err(|yaml_err| EnvSetError::Yaml(yaml_err))
+        })
     }
 }
